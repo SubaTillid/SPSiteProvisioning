611a7613e9e13e1b9a89b3b4eaefb4d734414f80
SubaTillid <subashree@tillidsoft.com>
2020-01-02 14:44:20 +0530

    Add project files.

---
+++ SPSiteProvisioning/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.SharePoint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+namespace SPSiteProvisioning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userName = ConfigurationSettings.AppSettings["username"];
+            var password = ConfigurationSettings.AppSettings["password"];
+            Console.WriteLine("userName" + userName);
+            Console.WriteLine("password" + password);
+            Console.ReadLine();            
+        }
+
+        public void createSite()
+        {
+
+        }
+        
+    }
+
+    
+}
