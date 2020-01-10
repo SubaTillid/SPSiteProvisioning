9f0293001f80218a15fb630f9071ae66d924c9e8
SubaTillid <subashree@tillidsoft.com>
2020-01-10 18:17:45 +0530

    Workflow provisioning,propertybag , spfx extension

--- SPSiteProvisioningWebApi/Controllers/HomeController.cs
+++ SPSiteProvisioningWebApi/Controllers/HomeController.cs
@@ -9,67 +9,89 @@ using System.Web.Mvc;
 using Microsoft.SharePoint.Client.Publishing;
 using SPSiteProvisioningWebApi.Models;
 using System.IO;
+using SPSiteProvisioningWebApi.Utils;
+using SPSiteProvisioningWebApi.Services;
 
 namespace SPSiteProvisioningWebApi.Controllers
 {
     public class HomeController : Controller
     {
+        private static ClientContext templateSiteClientContext;
+
+        private static ClientContext binderSiteClientContext;
+
+        private static string templateSiteUrl;
+
+        private static string binderSiteUrl;
+
+        private static SecureString passWord;
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-            getAllWorkFlows();
+            passWord = new SecureString();
+            foreach (char c in "n@#eTD@098!".ToCharArray())
+            {
+                passWord.AppendChar(c);
+            };
+            templateSiteUrl = "https://chennaitillidsoft.sharepoint.com/sites/developer5";
+            binderSiteUrl = "https://chennaitillidsoft.sharepoint.com/sites/POC/SiteProvisioning";
+            SetPropertyBagValue();
             return View();
         }
         public ActionResult TestApp()
         {
             ViewBag.Title = "Home Page";
-            getAllWorkFlows();
+            passWord = new SecureString();
+            foreach (char c in "n@#eTD@098!".ToCharArray())
+            {
+                passWord.AppendChar(c);
+            };
+            ProvisionWorkFlow();
             return View();
         }
-        public static void getAllWorkFlows()
+        public static void ProvisionWorkFlow()
         {
-            var siteURl = "https://chennaitillidsoft.sharepoint.com/sites/developer5";
-            var context = new ClientContext(siteURl);
-            SecureString passWord = new SecureString();
-            foreach (char c in "n@#eTD@098!".ToCharArray()) passWord.AppendChar(c);
-            context.Credentials = new SharePointOnlineCredentials("murali@chennaitillidsoft.onmicrosoft.com", passWord);
+            templateSiteClientContext = new ClientContext(templateSiteUrl);
+            
+            templateSiteClientContext.Credentials = new SharePointOnlineCredentials("murali@chennaitillidsoft.onmicrosoft.com", passWord);
 
-            var workflowServicesManager = new WorkflowServicesManager(context, context.Web);
+            var workflowServicesManager = new WorkflowServicesManager(templateSiteClientContext, templateSiteClientContext.Web);
 
             // connect to the deployment service 
             var workflowDeploymentService = workflowServicesManager.GetWorkflowDeploymentService();
 
             // get all installed workflows
             var publishedWorkflowDefinitions = workflowDeploymentService.EnumerateDefinitions(true);
-            context.Load(publishedWorkflowDefinitions);
-            context.ExecuteQuery();
+            templateSiteClientContext.Load(publishedWorkflowDefinitions);
+            templateSiteClientContext.ExecuteQuery();
 
             // display list of all installed workflows
             WorkflowDefinition currentWorkFlow = publishedWorkflowDefinitions.Where(flow => flow.DisplayName.Equals("BBH Document Atestation")).First();
-            if(currentWorkFlow != null)
+            if (currentWorkFlow != null)
             {
                 var workFlowTemplate = currentWorkFlow;
-                var workflowSubscriptionService = workflowServicesManager.GetWorkflowSubscriptionService();
+                //var workflowSubscriptionService = workflowServicesManager.GetWorkflowSubscriptionService();
 
-                // get all workflow associations
-                var workflowAssociations = workflowSubscriptionService.EnumerateSubscriptionsByDefinition(currentWorkFlow.Id);
-                context.Load(workflowAssociations);
-                context.ExecuteQuery();
+                //// get all workflow associations
+                //var workflowAssociations = workflowSubscriptionService.EnumerateSubscriptionsByDefinition(currentWorkFlow.Id);
+                //templateSiteClientContext.Load(workflowAssociations);
+                //templateSiteClientContext.ExecuteQuery();
 
-                foreach (var association in workflowAssociations)
-                {
-                    Console.WriteLine("{0} - {1}",
-                      association.Id, association.Name);
+                //foreach (var association in workflowAssociations)
+                //{
+                //    Console.WriteLine("{0} - {1}",
+                //      association.Id, association.Name);
 
-                }
-                var binderSiteUrl = "https://chennaitillidsoft.sharepoint.com/sites/POC/spotlight/HeroControlDev/";
-                ClientContext clientContext = new ClientContext(binderSiteUrl);
-                clientContext.Credentials = new SharePointOnlineCredentials("murali@chennaitillidsoft.onmicrosoft.com", passWord);
+                //}
+                //var binderSiteUrl = "https://chennaitillidsoft.sharepoint.com/sites/POC/spotlight/HeroControlDev/";
+                binderSiteClientContext = new ClientContext(binderSiteUrl);
+                binderSiteClientContext.Credentials = new SharePointOnlineCredentials("murali@chennaitillidsoft.onmicrosoft.com", passWord);
 
 
                 //Construct object with workflow template info
                 WorkflowTemplateInfo solutionInfo = new WorkflowTemplateInfo();
-                var solutionPath = "../../SiteAssets/BBH Document Atestation.wsp";
+                var solutionPath = "https://chennaitillidsoft.sharepoint.com/sites/developer5/SiteAssets/BBH Document Atestation.wsp";
                 solutionInfo.PackageFilePath = solutionPath;
                 //PackageName is mandatory
                 solutionInfo.PackageName = Path.GetFileNameWithoutExtension(solutionPath);
@@ -78,7 +100,7 @@ namespace SPSiteProvisioningWebApi.Controllers
                 //Workflow feature Id is need to activate workflow in the web
                 //solutionInfo.FeatureId = workFlowTemplate.;
                 //Init workflow template deployer
-                using (AddWorkFlowFormExistingTemplate workflowDeployer = new AddWorkFlowFormExistingTemplate(context))
+                using (AddWorkFlowFormExistingTemplate workflowDeployer = new AddWorkFlowFormExistingTemplate(binderSiteClientContext))
                 {
                     //Provisioning workflow resources
                     workflowDeployer.DeployWorkflowSolution(solutionPath);
@@ -88,5 +110,82 @@ namespace SPSiteProvisioningWebApi.Controllers
             }
             Console.ReadLine();
         }
+
+        public static void GetWorkFlowTemplate()
+        {
+            var templateSiteClientContext = new ClientContext(templateSiteUrl);
+
+            templateSiteClientContext.Credentials = new SharePointOnlineCredentials("murali@chennaitillidsoft.onmicrosoft.com", passWord);
+
+            var workflowServicesManager = new WorkflowServicesManager(templateSiteClientContext, templateSiteClientContext.Web);
+
+            // connect to the deployment service 
+            var workflowDeploymentService = workflowServicesManager.GetWorkflowDeploymentService();
+
+            // get all installed workflows
+            var publishedWorkflowDefinitions = workflowDeploymentService.EnumerateDefinitions(true);
+            templateSiteClientContext.Load(publishedWorkflowDefinitions);
+            templateSiteClientContext.ExecuteQuery();
+
+            // display list of all installed workflows
+            WorkflowDefinition currentWorkFlow = publishedWorkflowDefinitions.Where(flow => flow.DisplayName.Equals("BBH Document Atestation")).First();
+            ProvisionWorkFlowAndRelatedList(currentWorkFlow.Xaml, binderSiteUrl);
+        }
+
+        public static void ProvisionWorkFlowAndRelatedList(string workFlowXMlFile , string binderSiteUrl)
+        {
+            //Return the list the workflow will be associated with
+            binderSiteClientContext = new ClientContext(binderSiteUrl);
+            binderSiteClientContext.Credentials = new SharePointOnlineCredentials("murali@chennaitillidsoft.onmicrosoft.com", passWord);
+            var bbhDocumentList = CSOMUtil.GetListByTitle(binderSiteClientContext, "BBH Documents");
+
+            //Create a new WorkflowProvisionService class instance which uses the 
+            //Microsoft.SharePoint.Client.WorkflowServices.WorkflowServicesManager to
+            //provision and configure workflows
+            var service = new WorkflowProvisionService(binderSiteClientContext);
+
+            //Read the workflow .XAML file
+            //var bbhDocumentWF = System.IO.File.ReadAllText(workFlowXMlFile);
+            //string invalid = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            //foreach (char c in invalid)
+            //{
+            //    workFlowXMlFile = workFlowXMlFile.Replace(c.ToString(), "");
+            //}
+            //var solutionPath = "E:/Site Provisioning files/BBH Document Atestation (1).wsp";
+            var bbhDocumentWF = workFlowXMlFile;
+
+
+            //Create the WorkflowDefinition and use the 
+            //Microsoft.SharePoint.Client.WorkflowServices.WorkflowServicesManager
+            //to save and publish it.  
+            //This method is shown below for reference.
+            //var bbhDocumentWFDefinitionId = service.SaveDefinitionAndPublish("BBHDocument", WorkflowUtil.TranslateWorkflow(bbhDocumentWF));
+            var bbhDocumentWFDefinitionId = service.SaveDefinitionAndPublish("BBHDocument",bbhDocumentWF);
+
+            //Create the workflow tasks list
+            //var taskListId = service.CreateTaskList("BBHDocument Workflow Tasks");
+            var taskListId =  CSOMUtil.GetListByTitle(binderSiteClientContext, "BBHDocument Workflow Tasks");
+            //Create the workflow history list
+            var historyListId = service.CreateHistoryList("BBHDocument Workflow History");
+
+            //Use the Microsoft.SharePoint.Client.WorkflowServices.WorkflowSubscriptionService to 
+            //subscibe the workflow to the list the workflow is associated with, register the
+            //events it is associated with, and register the tasks and history list. 
+            //This method is shown below for reference.
+            service.Subscribe("BBHDocument Workflow", bbhDocumentWFDefinitionId, bbhDocumentList.Id,
+                WorkflowSubscritpionEventType.ItemAdded, taskListId.Id, historyListId);
+        }
+
+        public static void SetPropertyBagValue()
+        {
+            using (binderSiteClientContext = new ClientContext(binderSiteUrl))
+            {
+                binderSiteClientContext.Credentials = new SharePointOnlineCredentials("murali@chennaitillidsoft.onmicrosoft.com", passWord);
+                binderSiteClientContext.Web.SetPropertyBagValue("Test Update Property Bag Value 1", "Successfully Updated");
+                binderSiteClientContext.ExecuteQuery();
+                var propertyBagValue = binderSiteClientContext.Web.GetPropertyBagValueString("Test Update Property Bag Value", "Not Found");
+            }
+        }
     }
 }
+
---
+++ SPSiteProvisioningWebApi/Utils/CSOMUtil.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SPSiteProvisioningWebApi.Utils
+{
+    public class CSOMUtil
+    {
+        public static List GetListByTitle(ClientContext clientContext, string listTitle)
+        {
+            // Get the list collection for the website
+            var listColl = clientContext.Web.Lists;
+
+            var q = clientContext.LoadQuery<List>(listColl.Where(n => n.Title == listTitle));
+            clientContext.ExecuteQuery();
+
+            if (q.Count() > 0)
+            {
+                return q.FirstOrDefault<List>();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
